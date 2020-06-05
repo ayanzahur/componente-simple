be1a3493488ab97984c81bce223046f1a6fcd8c8
ayanzahur <andreavyanzah@hotmail.com>
2020-06-04 23:30:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# componente-simple
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/componente-simple)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';// <-- NgModel lives here
+
+import { AppComponent } from './app.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, EstudiantesComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
+
